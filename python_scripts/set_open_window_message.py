1a7ce19515e5dd991767c207d1d6447ef46bfcc1
Sean Straus <scstraus@gmail.com>
2020-09-10 00:17:29 +0000

    Window open message working now

--- python_scripts/set_open_window_message.py
+++ python_scripts/set_open_window_message.py
@@ -1,60 +1,15 @@
+#####################################################
+# This script just makes a message to display on my #
+# frontend for when there are windows open that are #
+# making the house cold or hot                      #
+#####################################################
 
-#LOW_TEMP = float((hass.states.get("sensor.dark_sky_overnight_low_temperature_0d")).state)
-#HIGH_TEMP = float((hass.states.get("sensor.dark_sky_daytime_high_temperature_0d")).state)
-#CLOUDY_DAY = ((hass.states.get("sensor.dark_sky_icon_0d")).state in ['partly-cloudy-day', 'rain', 'snow', 'sleet', 'wind', 'fog', 'cloudy'])
-#SUNNY_DAY = ((hass.states.get("sensor.dark_sky_icon_0d")).state in ['clear-day'])
 
-#TEMP_SET_SERVICE_DOMAIN = "evohome"
-#TEMP_SET_SERVICE_CALL = "set_zone_override"
-#SET_AUTO_SERVICE_DOMAIN = "evohome"
-#SET_AUTO_SERVICE_CALL = "clear_zone_override"
-
-#MIN_TEMP = 5
-#MAX_TEMP = 25
+###### Global Variables #######
 
 WINDOW_ENTITIES= ["binary_sensor.back_door", "binary_sensor.sebastians_room_left_window", "binary_sensor.sophies_room_ceiling_window", "binary_sensor.ecolink_closet_left_window", "binary_sensor.laundry_room_window_zone_13", "binary_sensor.ecolink_garage_door"]
 
-#
-#
-#SKIP_TOKEN = 0
-#AUTO_TOKEN = 100
-#
-#OVERRIDE_MINUTES = 1439
-
-
-
-## PROFILES DEFINED HERE ##
-
-
-
-#PROFILE_AUTO = [100,100,100,100,100,100,100,100,100,100,100,100]
-#PROFILE_A = [20,18,18,21,0,22,21,22,22,22,21,100]
-#PROFILE_B = [19,17,17,20,0,21,21,21,21,21,21,21]
-#PROFILE_C = [18,16,15,18,16,20,20,20,20,21,21,21]
-#PROFILE_D = [15,15,15,15,15,15,15,15,15,18,18,19]
-#PROFILE_E = [15,15,15,15,15,15,15,15,15,15,15,12]
-#
-#
-#LIVING_ROOM_18 = [0,0,0,0,18,0,0,0,0,0,0,0]
-#LIVING_ROOM_20 = [0,0,0,0,20,0,0,0,0,0,0,0]
-#LIVING_ROOM_AUTO = [0,0,0,0,100,0,0,0,0,0,0,0]
-#
-#
-#AUTOMATION_ENTITIES = ["automation.working_from_home_today", "automation.working_from_home_now", "automation.turn_up_the_heat_in_sophie_s_room_if_she_s_home"]
-
-## 1 is on, 0 is off
-
-#AUTOMATIONS_PROFILE_AUTO = [1,1,1]
-#AUTOMATIONS_PROFILE_A = [1,1,1]
-#AUTOMATIONS_PROFILE_B = [1,1,1]
-#AUTOMATIONS_PROFILE_C = [1,1,1]
-#AUTOMATIONS_PROFILE_D = [0,0,0]
-#AUTOMATIONS_PROFILE_E = [0,0,0]
-
-
-
-###### Function definitions and startup. Proably don't need to change these ######
-
+###### Function definitions ######
 
 def count_open_windows():
   num_open_windows = 0
@@ -69,153 +24,56 @@ def count_open_windows():
   logger.debug("Total number of open windows: %s",num_open_windows)
   return num_open_windows
 
-def make_open_windows_message(num_open_windows)
+def make_open_windows_message(num_open_windows):
   counter = num_open_windows
   window_counter = 0
   message=""
-  if (hass.states.get("binary_sensor.back_door") == "on":
-    message="The back door"
+  if (hass.states.get("binary_sensor.back_door").state) == "on":
+    message="The open back door"
+    counter = counter - 1
+  if (hass.states.get("binary_sensor.ecolink_garage_door").state) == "on":
+    if num_open_windows > counter:
+      message = message + ", the open garage door"
+    else:
+      message = "The open garage door"
     counter = counter - 1
   if num_open_windows > counter:
-    message = message + ", and windows in"
-  elif num_open_windows > 1: 
-    message = message + "Windows in"
-  if (hass.states.get("binary_sensor.sebastians_room_left_window") == "on":
+    message = message + ", and open windows in"
+  else:
+    message = message + "Open windows in"
+  if (hass.states.get("binary_sensor.sebastians_room_left_window").state) == "on":
     message = message + " Sebastian's Room"
     counter = counter - 1
     window_counter = window_counter + 1
-  if (hass.states.get("binary_sensor.sophies_room_ceiling_window") == "on":
+  if (hass.states.get("binary_sensor.sophies_room_ceiling_window").state) == "on":
+    if counter == 1 and window_counter > 0:
+      message = message + ", and"
+    elif window_counter > 0:
+      message = message + ", "
     message = message + " Sophie's Room"
-    counter = counter - 1  
+    counter = counter - 1
+    window_counter = window_counter + 1
+  if (hass.states.get("binary_sensor.ecolink_closet_left_window").state) == "on":
+    if counter == 1 and window_counter > 0:
+      message = message + ", and the"
+    elif window_counter > 0:
+      message = message + ", "
+    message = message + " Master Closet"
+    counter = counter - 1
+    window_counter = window_counter + 1  
+  if (hass.states.get("binary_sensor.laundry_room_window_zone_13").state) == "on":
+    if counter == 1 and window_counter > 0:
+      message = message + ", and the"
+    elif window_counter > 0:
+      message = message + ", "
+    message = message + " Laundry Room"
+    counter = counter - 1
+    window_counter = window_counter + 1  
+  message = message + " are making the house cold."
+  return message
 
-"binary_sensor.ecolink_closet_left_window", "binary_sensor.laundry_room_window_zone_13", "binary_sensor.ecolink_garage_door"]
+###### Run the Functions ######
 
-    
-
-#logger.info("Weather Description: %s", (
-#  (hass.states.get("sensor.dark_sky_icon_0d")).state))
-
-## Function that's actually setting the temperatures ##
-#def set_to_profile(profile,profile_name,automations_profile,automations_profile_name):
-#  logger.debug("Setting to profile %s",profile)
-#
-#  ## turn off automations that aren't needed for profile ##
-#  if automations_profile_name in ["AUTOMATIONS_PROFILE_AUTO", "AUTOMATIONS_PROFILE_A", "AUTOMATIONS_PROFILE_B", "AUTOMATIONS_PROFILE_C", "AUTOMATIONS_PROFILE_D", "AUTOMATIONS_PROFILE_E"]: 
-#    for count in range(len(automations_profile)):
-#      logger.debug("Working on automation item %s",count)
-#      logger.debug("Heat entity name: %s", AUTOMATION_ENTITIES[count])
-#      logger.debug("Set to: %s", automations_profile[count])
-#      if automations_profile[count]==0:
-#        hass.services.call("automation", "turn_off", {"entity_id" : AUTOMATION_ENTITIES[count]}, False)
-#      elif automations_profile[count]==1:
-#        hass.services.call("automation", "turn_on", {"entity_id" : AUTOMATION_ENTITIES[count]}, False)
-#  elif automations_profile_name=="NULL":
-#    logger.debug("Living Room Heating Procfile, not setting automations on/off")
-#  else:
-#    logger.debug("Bad value for automation profile name: %s",automations_profile)
-
-  ## set temperature ##
-  
-  #  for count,setting in enumerate(profile):
-#  for count in range(len(profile)):
-#    logger.debug("Working on heat item %s",count)
-#    logger.debug("Heat entity name: %s", ROOM_ENTITIES[count])
-#    logger.debug("Set to: %s", profile[count])
-#    if profile_name in ["PROFILE_AUTO", "PROFILE_A", "PROFILE_B", "PROFILE_C", "PROFILE_D", "PROFILE_E"]: 
-#      hass.services.call("input_select", "select_option", {"entity_id" : "input_select.active_heating_profile", "option" : profile_name}, False)
-#    if profile_name in ["LIVING_ROOM_18", "LIVING_ROOM_20", "LIVING_ROOM_AUTO"]:  
-#      hass.services.call("input_select", "select_option", {"entity_id" : "input_select.active_heating_profile_living_room", "option" : profile_name}, False)    
-#    if profile[count]==SKIP_TOKEN:
-#      logger.debug("Not setting %s",ROOM_ENTITIES[count]) 
-#    elif profile[count]==AUTO_TOKEN:
-#      logger.debug("Setting %s to Auto", ROOM_ENTITIES[count])  
-#      hass.services.call(SET_AUTO_SERVICE_DOMAIN, SET_AUTO_SERVICE_CALL, {"entity_id" : ROOM_ENTITIES[count]}, False)
-#    elif profile[count] in range(MIN_TEMP, MAX_TEMP):
-#      logger.debug("Setting %s to %s for 1439 minutes", ROOM_ENTITIES[count], profile[count])
-#      hass.services.call(TEMP_SET_SERVICE_DOMAIN, TEMP_SET_SERVICE_CALL, {"entity_id" : ROOM_ENTITIES[count], "setpoint" : profile[count], "duration" : {"minutes":1439}}, False)
-#    else:
-#      logger.info("Invalid value (%s) for temperature for room %s. Must be in range %s-%s. Item #%s skipped.", setpoint, ROOM_ENTITIES[count], MAX_TEMP, MIN_TEMP, count)
-
-
-## Just some debug info in case something doesn't work ##
-#def startup_log_dump():
-#  logger.info("Starting up climate profile python script")
-#  logger.info("Low temp: %s",LOW_TEMP)
-#  logger.info("High temp: %s",HIGH_TEMP)
-#  logger.info("Weather Description: %s", (
-#  (hass.states.get("sensor.dark_sky_icon_0d")).state))
-#  logger.info("Is cloudy: %s",CLOUDY_DAY)
-#  logger.info("Is sunny: %s",SUNNY_DAY)
-#     
-#startup_log_dump()
-
-count_open_windows()
-
-########################################################################
-## These are the rules for which profile to use based on the weather, ##
-## you can change these based on any rules you want or try to keep    ##
-## them as a starting point to fine tune your temps. I just made them ## 
-## based on how my house felt under different conditions, so they     ##
-## work well for me, but YMMV                                         ##
-########################################################################
-
-## Some general rules for in case we don't match others ##
-#if LOW_TEMP > 14 and HIGH_TEMP > 25:
-#  logger.info("Low > 14 High > 25 - Choose Profile E")
-#  set_to_profile(PROFILE_E,"PROFILE_E",AUTOMATIONS_PROFILE_E,"AUTOMATIONS_PROFILE_E")
-#elif LOW_TEMP >= 12 and LOW_TEMP <= 14 and HIGH_TEMP > 23:
-#  logger.info("Low 12-14 High > 23 - Choose Profile D")
-#  set_to_profile(PROFILE_D,"PROFILE_D",AUTOMATIONS_PROFILE_D,"AUTOMATIONS_PROFILE_D")
-#elif LOW_TEMP >= 10 and LOW_TEMP <= 13 and HIGH_TEMP > 20:
-#  logger.info("Low 10-13 High <= 13 - Choose Profile C")
-#  set_to_profile(PROFILE_C,"PROFILE_C",AUTOMATIONS_PROFILE_C,"AUTOMATIONS_PROFILE_C")
-###  Rules for cool sunny weather ##
-#elif SUNNY_DAY and LOW_TEMP <= 5 and HIGH_TEMP >= 13:
-#  logger.info("Sunny Low <=5 High >= 13 - Choose Profile A")
-#  set_to_profile(PROFILE_A,"PROFILE_A",AUTOMATIONS_PROFILE_A,"AUTOMATIONS_PROFILE_A")
-#elif SUNNY_DAY and LOW_TEMP > 5 and LOW_TEMP <= 10 and HIGH_TEMP >= 13 and HIGH_TEMP <= 20:
-#  logger.info("Sunny Low 5-10 High 13-20 - Choose Profile A")
-#  set_to_profile(PROFILE_A,"PROFILE_A",AUTOMATIONS_PROFILE_A,"AUTOMATIONS_PROFILE_A")
-#elif SUNNY_DAY and LOW_TEMP > 5 and LOW_TEMP <= 10 and HIGH_TEMP > 20:
-#  logger.info("Sunny Low 5-10 High > 20 - Choose Profile B")
-#  set_to_profile(PROFILE_B,"PROFILE_B",AUTOMATIONS_PROFILE_B,"AUTOMATIONS_PROFILE_B")
-#elif SUNNY_DAY and LOW_TEMP < 10 and HIGH_TEMP < 13:
-#  logger.info("Sunny Low < 10 High < 13 - Choose Profile Auto")
-#  set_to_profile(PROFILE_AUTO,"PROFILE_AUTO",AUTOMATIONS_PROFILE_AUTO,"AUTOMATIONS_PROFILE_AUTO")
-### Riles for cool cloudy weather ##
-#elif CLOUDY_DAY and LOW_TEMP >= 7 and LOW_TEMP <= 10 and HIGH_TEMP >= 15 and HIGH_TEMP <= 20:
-#  logger.info("Cloudy Low 7-10 High 15-20 - Choose Profile A")
-#  set_to_profile(PROFILE_A,"PROFILE_A",AUTOMATIONS_PROFILE_A,"AUTOMATIONS_PROFILE_A")
-#elif CLOUDY_DAY and LOW_TEMP < 7 and HIGH_TEMP >= 15:
-#  logger.info("Cloudy Low < 7 High > 15 - Choose Profile A")
-#  set_to_profile(PROFILE_A,"PROFILE_A",AUTOMATIONS_PROFILE_A,"AUTOMATIONS_PROFILE_A")
-#elif CLOUDY_DAY and LOW_TEMP >= 7 and LOW_TEMP <= 10 and HIGH_TEMP > 20:
-#  logger.info("Cloudy Low 7-10 High > 20 - Choose Profile B")
-#  set_to_profile(PROFILE_B,"PROFILE_B",AUTOMATIONS_PROFILE_B,"AUTOMATIONS_PROFILE_B")
-#elif CLOUDY_DAY and LOW_TEMP < 7 and HIGH_TEMP < 15:
-#  logger.info("Cloudy Low < 7 High < 15 - Choose Profile Auto")
-#  set_to_profile(PROFILE_AUTO,"PROFILE_AUTO",AUTOMATIONS_PROFILE_AUTO,"AUTOMATIONS_PROFILE_AUTO")
-### Now, in theory the above rules covered all cases, this one is just here in case I screwed up (which is likely) ##
-#else:
-#  set_to_profile(PROFILE_AUTO,"PROFILE_AUTO",AUTOMATIONS_PROFILE_AUTO,"AUTOMATIONS_PROFILE_AUTO")
-#  logger.error("Didn't match any rules")
-
-#########################################################################################
-## I like to turn down the living room based on different rules because it has lots of ##
-## west facing windows and the temperature is very sensitive to the sun, you could get ##
-## rid of the below if the rules above and normal profiles are enough                  ##
-#########################################################################################
-#
-#if LOW_TEMP >= 2 and LOW_TEMP <= 5 and HIGH_TEMP > 11:
-#  logger.info("Low 2-5 High > 11 - Choose Living Room 20")
-#  set_to_profile(LIVING_ROOM_20,"LIVING_ROOM_20",NULL,"NULL")
-#elif LOW_TEMP > 5 and LOW_TEMP < 10 and HIGH_TEMP > 13:
-#  logger.info("Low 5-10 High > 13 - Choose Living Room 18")
-#  set_to_profile(LIVING_ROOM_18,"LIVING_ROOM_18",NULL,"NULL")
-#elif LOW_TEMP < 2 and HIGH_TEMP > 11: 
-#  logger.info("Low < 2 High > 11 - Choose Living Room Auto")
-#  set_to_profile(LIVING_ROOM_AUTO,"LIVING_ROOM_AUTO",NULL,"NULL")
-#elif LOW_TEMP < 5 and HIGH_TEMP < 11:
-#  logger.info("Low <= 5 High <= 11 - Choose Living Room Auto")
-#  set_to_profile(LIVING_ROOM_AUTO,"LIVING_ROOM_AUTO",NULL,"NULL")
+message=make_open_windows_message(count_open_windows())
+logger.debug("The message is: %s",message)
 
